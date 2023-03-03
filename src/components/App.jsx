3cc938731ea5bed34447785fbfed27a07637a844
Kevin <kpeibanez@gmail.com>
2023-03-02 18:56:18 -0600

    Initialized the React project with proper folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import useStyles from './styles';
+import NavBar from './NavBar/NavBar';
+import Movies from './Movies/Movies';
+import Actors from './Actors/Actors';
+import MovieInformation from './MovieInformation/MovieInformation';
+import Profile from './Profile/Profile';
 
-const App = () => (
-  <div>
-    App
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NavBar = () => {
+  console.log('hi');
+  return <div>NavBar</div>;
+};
+
+export default NavBar;
